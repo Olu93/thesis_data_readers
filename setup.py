16e23ab2454de7f9579ba0951bd4619e3351dc9e
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-01-05 00:25:08 +0100

    Impl: better packging according to guide
    https://packaging.python.org/en/latest/tutorials/installing-packages/

--- setup.py
+++ setup.py
@@ -5,22 +5,27 @@ classifiers = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Science',
     'License :: OSI Approved :: GPL v3 License',
-    'Operating System :: Microsoft :: Windows',
+    'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
 ]
 
 setup(
     name='thesis_data_readers',
     version='0.0.1',
-    description='A reader package for specific logs within process mining',
-    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
-    url='',
-    classifiers=classifiers,
-    packages=find_packages(),
-    install_requires=open('requirements.txt').read().split('\n'),
     author="Olusanmi Hundogan",
     author_email="o.hundogan@gmail.com",
-    license="GPL",
-    python_requires='>=3',
+    description='A reader package for specific logs within process mining',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/Olu93/thesis_data_readers',
+    project_urls={
+        "Bug Tracker": "https://github.com/Olu93/thesis_data_readers/issues",
+    },
+    classifiers=classifiers,
+    package_dir={"": "src"},
+
+    packages=find_packages(where="src"),
+    install_requires=open('requirements.txt').read().split('\n'),
+    python_requires='>=3.6',
     keywords="reader, xes, log, BPIC",
 )
