19fbf78c51dbc107ac72b0f7d627245fcf3c0da4
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-01-05 02:13:33 +0100

    Impl: modern packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import pathlib
 # Follows https://www.youtube.com/watch?v=zhpI6Yhz9_4
 # Follows https://packaging.python.org/en/latest/tutorials/packaging-projects/
 
@@ -9,14 +10,16 @@ classifiers = [
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
 ]
+CURR_DIR = pathlib.Path('.')
 
+print(__package__)
 setup(
     name='thesis_data_readers',
     version='0.0.1',
     author="Olusanmi Hundogan",
     author_email="o.hundogan@gmail.com",
     description='A reader package for specific logs within process mining',
-    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description=open(CURR_DIR / 'README.md').read() + '\n\n' + open(CURR_DIR / 'CHANGELOG.md').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/Olu93/thesis_data_readers',
     project_urls={
@@ -24,9 +27,8 @@ setup(
     },
     classifiers=classifiers,
     package_dir={"": "src"},
-
     packages=find_packages(where="src"),
-    install_requires=open('requirements.txt').read().split('\n'),
+    install_requires=open(CURR_DIR / 'requirements.txt').read().split('\n'),
     python_requires='>=3.6',
     keywords="reader, xes, log, BPIC",
 )
